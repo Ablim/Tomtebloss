c1a9ee79032e502c091b5c864e9beed364080cb8
Albin <a.bramstang@gmail.com>
2012-12-04 00:31:43 +0100

    Added a button

--- src/com/bramstang/tomtebloss/TomteblossMain.java
+++ src/com/bramstang/tomtebloss/TomteblossMain.java
@@ -1,8 +1,10 @@
 package com.bramstang.tomtebloss;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class TomteblossMain extends Activity {
 
@@ -16,7 +18,13 @@ public class TomteblossMain extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_tomtebloss_main, menu);
+		
+		Button button = (Button) findViewById(R.id.spark_button);
+        button.setOnClickListener(new View.OnClickListener()  {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
 		return true;
 	}
-
 }
