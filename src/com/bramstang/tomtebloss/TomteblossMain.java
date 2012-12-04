a9f0b3b7ef5fef9d326c4d4934f122afc0525065
Albin <a.bramstang@gmail.com>
2012-12-04 23:40:15 +0100

    Added a clickable candle.

--- src/com/bramstang/tomtebloss/TomteblossMain.java
+++ src/com/bramstang/tomtebloss/TomteblossMain.java
@@ -1,12 +1,16 @@
 package com.bramstang.tomtebloss;
 
 import android.app.Activity;
+import android.content.ClipData.Item;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class TomteblossMain extends Activity {
+	
+	private boolean isOn = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +23,28 @@ public class TomteblossMain extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_tomtebloss_main, menu);
 		
-		Button button = (Button) findViewById(R.id.spark_button);
-        button.setOnClickListener(new View.OnClickListener()  {
-            public void onClick(View v) {
-                // Perform action on click
-            }
-        });
+		
+//		final menu settingsButton = (Button) findViewById(R.id.spark_button);
+//        button.setOnClickListener(new View.OnClickListener()  {
+//            public void onClick(View v) {
+//                // Perform action on click
+//            }
+//        });
+		
+		final ImageButton sparkButton = (ImageButton) findViewById(R.id.spark_button);
+		sparkButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (isOn) {
+					sparkButton.setImageResource(R.drawable.candle_off);
+					isOn = !isOn;
+				} else {
+					sparkButton.setImageResource(R.drawable.candle_on);
+					isOn = !isOn;
+				}
+			}
+		});
 		return true;
 	}
 }
