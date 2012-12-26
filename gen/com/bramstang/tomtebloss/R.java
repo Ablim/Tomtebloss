dcc2459a633d520194c82e13bbdfc17b7e748d16
Albin <albin@albinlaptop.(none)>
2012-12-26 16:57:44 +0100

    Added images of real sparklers.

--- gen/com/bramstang/tomtebloss/R.java
+++ gen/com/bramstang/tomtebloss/R.java
@@ -11,9 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int candle_off=0x7f020000;
-        public static final int candle_on=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ic_launcher=0x7f020000;
+        public static final int spark1=0x7f020001;
+        public static final int spark2=0x7f020002;
         public static final int spark_icon=0x7f020003;
     }
     public static final class id {
@@ -28,6 +28,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int content_description=0x7f040003;
         public static final int menu_settings=0x7f040001;
         public static final int spark=0x7f040002;
     }
--- src/com/bramstang/tomtebloss/TomteblossMain.java
+++ src/com/bramstang/tomtebloss/TomteblossMain.java
@@ -1,10 +1,7 @@
 package com.bramstang.tomtebloss;
 
 import android.app.Activity;
-import android.content.ClipData.Item;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -16,20 +13,6 @@ public class TomteblossMain extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tomtebloss_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_tomtebloss_main, menu);
-		
-		
-//		final menu settingsButton = (Button) findViewById(R.id.spark_button);
-//        button.setOnClickListener(new View.OnClickListener()  {
-//            public void onClick(View v) {
-//                // Perform action on click
-//            }
-//        });
 		
 		final ImageButton sparkButton = (ImageButton) findViewById(R.id.spark_button);
 		sparkButton.setOnClickListener(new View.OnClickListener() {
@@ -37,14 +20,20 @@ public class TomteblossMain extends Activity {
 			@Override
 			public void onClick(View v) {
 				if (isOn) {
-					sparkButton.setImageResource(R.drawable.candle_off);
+					sparkButton.setImageResource(R.drawable.spark1);
 					isOn = !isOn;
 				} else {
-					sparkButton.setImageResource(R.drawable.candle_on);
+					sparkButton.setImageResource(R.drawable.spark2);
 					isOn = !isOn;
 				}
 			}
 		});
-		return true;
 	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.activity_tomtebloss_main, menu);
+//		return true;
+//	}
 }
