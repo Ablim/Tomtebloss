4fab5ed7de6e86039529004522b670575828fc60
Albin <a.bramstang@gmail.com>
2013-02-07 23:48:24 +0100

    Tried to add a loop...

--- gen/com/bramstang/tomtebloss/R.java
+++ gen/com/bramstang/tomtebloss/R.java
@@ -13,8 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int spark1=0x7f020001;
-        public static final int spark2=0x7f020002;
-        public static final int spark_icon=0x7f020003;
+        public static final int spark10=0x7f020002;
+        public static final int spark11=0x7f020003;
+        public static final int spark12=0x7f020004;
+        public static final int spark13=0x7f020005;
+        public static final int spark14=0x7f020006;
+        public static final int spark15=0x7f020007;
+        public static final int spark2=0x7f020008;
+        public static final int spark3=0x7f020009;
+        public static final int spark4=0x7f02000a;
+        public static final int spark5=0x7f02000b;
+        public static final int spark6=0x7f02000c;
+        public static final int spark7=0x7f02000d;
+        public static final int spark8=0x7f02000e;
+        public static final int spark9=0x7f02000f;
+        public static final int spark_icon=0x7f020010;
     }
     public static final class id {
         public static final int menu_settings=0x7f070001;
--- src/com/bramstang/tomtebloss/TomteblossMain.java
+++ src/com/bramstang/tomtebloss/TomteblossMain.java
@@ -2,12 +2,11 @@ package com.bramstang.tomtebloss;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 
 public class TomteblossMain extends Activity {
 	
-	private boolean isOn = false;
+//	private boolean isOn = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +14,41 @@ public class TomteblossMain extends Activity {
 		setContentView(R.layout.activity_tomtebloss_main);
 		
 		final ImageButton sparkButton = (ImageButton) findViewById(R.id.spark_button);
-		sparkButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				if (isOn) {
-					sparkButton.setImageResource(R.drawable.spark1);
-					isOn = !isOn;
-				} else {
-					sparkButton.setImageResource(R.drawable.spark2);
-					isOn = !isOn;
+		
+		
+		while (true) {
+			int i = 1;
+			for (i = 1; i < 16; i++) {
+				try {
+					sparkButton.setImageResource(R.drawable.class.getField("spark" + i).getInt(0));
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (NoSuchFieldException e) {
+					e.printStackTrace();
+				}
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 			}
-		});
+		}
+//		sparkButton.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				if (isOn) {
+//					sparkButton.setImageResource(R.drawable.spark1);
+//					isOn = !isOn;
+//				} else {
+//					sparkButton.setImageResource(R.drawable.spark2);
+//					isOn = !isOn;
+//				}
+//			}
+//		});
 	}
 
 //	@Override
