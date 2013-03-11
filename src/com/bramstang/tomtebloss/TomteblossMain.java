66eef42f2c1098101c0c607ae76288910203106c
Albin <a.bramstang@gmail.com>
2013-03-11 21:56:39 +0100

    Created a working loop.

--- src/com/bramstang/tomtebloss/TomteblossMain.java
+++ src/com/bramstang/tomtebloss/TomteblossMain.java
@@ -1,60 +1,71 @@
 package com.bramstang.tomtebloss;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
 public class TomteblossMain extends Activity {
-	
-//	private boolean isOn = false;
+
+	private Timer timer;
+	private ImageButton sparkButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_tomtebloss_main);
-		
-		final ImageButton sparkButton = (ImageButton) findViewById(R.id.spark_button);
-		
-		
-		while (true) {
-			int i = 1;
-			for (i = 1; i < 16; i++) {
-				try {
-					sparkButton.setImageResource(R.drawable.class.getField("spark" + i).getInt(0));
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (NoSuchFieldException e) {
-					e.printStackTrace();
-				}
-				
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-//		sparkButton.setOnClickListener(new View.OnClickListener() {
-//			
-//			@Override
-//			public void onClick(View v) {
-//				if (isOn) {
-//					sparkButton.setImageResource(R.drawable.spark1);
-//					isOn = !isOn;
-//				} else {
-//					sparkButton.setImageResource(R.drawable.spark2);
-//					isOn = !isOn;
-//				}
-//			}
-//		});
+		sparkButton = (ImageButton) findViewById(R.id.spark_button);
+		sparkButton.setImageResource(R.drawable.spark1);
 	}
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.activity_tomtebloss_main, menu);
-//		return true;
-//	}
+	@Override
+	protected void onStop() {
+		super.onStop();
+		timer.cancel();
+		timer.purge();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		timer = new Timer();
+		timer.schedule(new ImageTask(), 75, 75);
+	}
+
+	private class ImageTask extends TimerTask {
+
+		private int index;
+		private int maxIndex;
+		private AtomicBoolean lock;
+
+		public ImageTask() {
+			index = 1;
+			maxIndex = 15;
+			lock = new AtomicBoolean();
+		}
+
+		@Override
+		public void run() {
+			if (!lock.get()) {
+				runOnUiThread(new Runnable() {
+					public void run() {
+						lock.set(true);
+						Resources r = getResources();
+//						sparkButton.setImageResource(getResources().getIdentifier("spark" + index++, "drawable", "com.bramstang.tomtebloss"));
+						sparkButton.setImageDrawable(r.getDrawable(r.getIdentifier("spark" + index++, "drawable", "com.bramstang.tomtebloss")));
+						
+						if (index > maxIndex) {
+							index = 1;
+						}
+						lock.set(false);
+					}
+				});
+			} 
+		}
+	}
 }
